f3f1cdaf7d5729003fc69d6b526fa6034887c75f
Bradley White <bradleywhite93@gmail.com>
2026-06-28 00:10:12 +0100

    fix offline caching - resolve index.html against base path in service worker fetch handler

--- src/ChannelApp.Presentation/wwwroot/service-worker.published.js
+++ src/ChannelApp.Presentation/wwwroot/service-worker.published.js
@@ -40,13 +40,10 @@ async function onActivate(event) {
 async function onFetch(event) {
     let cachedResponse = null;
     if (event.request.method === 'GET') {
-        // For all navigation requests, try to serve index.html from cache,
-        // unless that request is for an offline resource.
-        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
         const shouldServeIndexHtml = event.request.mode === 'navigate'
             && !manifestUrlList.some(url => url === event.request.url);
 
-        const request = shouldServeIndexHtml ? 'index.html' : event.request;
+        const request = shouldServeIndexHtml ? new URL('index.html', baseUrl).href : event.request;
         const cache = await caches.open(cacheName);
         cachedResponse = await cache.match(request);
     }
